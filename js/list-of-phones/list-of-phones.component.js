c3e608517f0c2b9bfa319a994a3bd1832fccd7b3
Radion <radion91@gmail.com>
2016-09-06 19:58:52 +0300

    asdasd

--- js/list-of-phones/list-of-phones.component.js
+++ js/list-of-phones/list-of-phones.component.js
@@ -1,17 +1,27 @@
 angular.module('listOfPhones').component('listOfPhones',{
     templateUrl:'js/list-of-phones/list-of-phones.template.html',
-    controller: function listOfPhonesController(){
-        this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-    ];
+    controller: function listOfPhonesController($http){
+        // this.phones = [
+        // {
+        //   name: 'Nexus S',
+        //   snippet: 'Fast just got faster with Nexus S.',
+        //   age:1
+        // }, {
+        //   name: 'Motorola XOOM™ with Wi-Fi',
+        //   snippet: 'The Next, Next Generation tablet.',
+        //   age:3
+        // }, {
+        //   name: 'MOTOROLA XOOM™',
+        //   snippet: 'The Next, Next Generation tablet.',
+        //   age:2
+        // }
+        var self = this;
+        self.orderProp = 'age';
+
+        $http.get('phones/phones.json').then(function(resp){
+          self.phones = resp.data;
+        });
+
+    this.orderProp = 'age';
     }
 });
